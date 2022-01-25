ccd15c0ed7466b23843df23163f6197b2eb0ea04
JHONCARLOS20 <jhoncarlos_compras_123_@hotmail.com>
2022-01-25 00:39:03 -0500

    EC_03 Completa

--- app/src/main/java/com/example/evacont03/MainActivity.java
+++ app/src/main/java/com/example/evacont03/MainActivity.java
@@ -2,13 +2,27 @@ package com.example.evacont03;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button Personal_data;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Personal_data=(Button)findViewById(R.id.ventana2);
+
+        Personal_data.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent i =new Intent( MainActivity.this, Body.class);
+                startActivity(i);
+            }
+        });
     }
 }
